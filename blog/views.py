497e3c6e926a68ddafe5e517ef08600ffe65c7c1
muhammadsajjad <muhammadsajjad2710@gmail.com>
2023-08-24 18:13:45 +0500

    Initial commit

---
+++ blog/views.py
@@ -0,0 +1,34 @@
+#from django.shortcuts import render
+
+# Create your views here.
+# blog/views.py
+from django.views.generic import ListView, DetailView # new
+from django.views.generic.edit import CreateView, UpdateView, DeleteView # new
+from django.urls import reverse_lazy 
+from .models import Post
+
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+
+class BlogDetailView(DetailView): #new
+    model = Post
+    template_name = 'post_detail.html'
+    
+
+class BlogCreateView(CreateView): #new
+    model = Post
+    template_name = 'post_new.html'
+    fields = '__all__'
+    
+class BlogUpdateView(UpdateView): # new
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+class BlogDeleteView(DeleteView): 
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
+    
